46aa85c2fe4bc0cddb8eb640bb9440c97b7ebfc2
Taziva <stmarovatsanga@gmail.com>
2017-04-11 18:15:36 +0100

    Add first component called Flags

--- src/components/Flag.js
+++ src/components/Flag.js
@@ -32,6 +32,6 @@ export default class Flag extends React.Component {
         <img className="icon" title={name} src={`/img/${icon}`}/>
         {this.props.showName && <span className="name">{name}</span>}
       </span>
-    )
+    );
   }
 }
